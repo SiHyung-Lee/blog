ea3e5a1bebd8dfb45bcc862710fe8d571f971e9e
dev-athome <l_sh@athomecorp.com>
2023-02-02 18:52:04 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let post = "강남 우동 맛집";
+  let [title, setTitle] = useState([
+    "남자코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+  const [like, setLike] = useState(0);
+  const [modal, setModal] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>React Blog</h4>
+      </div>
+      <button
+        onClick={() => {
+          let array = [...title].sort();
+          setTitle(array);
+        }}
+      >
+        가나다라순 정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...title];
+          copy[0] = "여자코트 추천";
+          setTitle(copy);
+        }}
+      >
+        글수정
+      </button>
+      <div className="list">
+        <h4>
+          {title[0]} <span onClick={() => setLike(like + 1)}>👍</span> {like}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4 onClick={() => setModal(true)}>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      {modal ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
